07c5c4e4d3695c31d7f22ac6cd5536641fc21922
ishirihhhhhhh <131091996+ishirihhhhhhh@users.noreply.github.com>
2024-01-07 14:18:34 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,4 +1,5 @@
-function nazo1(){if(document.getElementById('answer1').value =='2015'){alert('正解！')
-}else{alert(不正解...);
-}
+function nazo1(){
+if(document.getElementById('answer1').value ==　'2015'){
+alert('正解！');}
+else{alert('不正解...');}
 }
